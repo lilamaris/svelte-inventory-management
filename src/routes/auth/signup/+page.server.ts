2a1625384f36671cedd22026c175b2c44b4bb4fb
lilamaris <lilamaris.dev@gmail.com>
2025-06-14 01:35:16 +0900

    feat: implement server-side signup logic and Svelte signup page with form handling and validation

---
+++ src/routes/auth/signup/+page.server.ts
@@ -0,0 +1,43 @@
+import type { Actions } from './$types';
+import { checkEmailAvailability, createUser } from '$lib/server/api/user';
+import { signupSchema } from '$features/auth/types/signupSchema';
+import type { ActionState } from '$lib/types';
+import { fail } from '@sveltejs/kit';
+
+export const actions: Actions = {
+    signup: async ({ request }) => {
+        const formData = Object.fromEntries(await request.formData());
+        const validateData = signupSchema.safeParse(formData);
+
+        if (!validateData.success) {
+            const state: ActionState<typeof signupSchema> = {
+                errors: validateData.error.flatten().fieldErrors,
+                payload: formData,
+                message: 'Invalid data'
+            };
+            return fail(400, { state });
+        }
+
+        const { username, email, password } = validateData.data;
+
+        if (!(await checkEmailAvailability(email))) {
+            const state: ActionState<typeof signupSchema> = {
+                errors: {
+                    email: ['Email already exists']
+                },
+                payload: formData,
+                message: 'Email already exists'
+            };
+            return fail(400, { state });
+        }
+
+        const user = await createUser(email, username, password);
+
+        return {
+            state: {
+                success: true,
+                message: 'Signup successful'
+            }
+        };
+    }
+};
---
+++ src/routes/auth/signup/+page.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    import * as Card from '$components/ui/card';
+
+    import SignupForm from '$features/auth/components/signup-form.svelte';
+    import Button from '$components/ui/button/button.svelte';
+
+    const formId = 'signup-form';
+    const action = '?/signup';
+
+    let { form } = $props();
+</script>
+
+<div class="bg-muted flex h-screen w-full items-center justify-center px-4">
+    <Card.Root class="mx-auto w-full max-w-sm">
+        <Card.Header>
+            <Card.Title class="text-2xl">Sign up</Card.Title>
+            <Card.Description>Create an account to get started</Card.Description>
+        </Card.Header>
+        <Card.Content>
+            <div class="flex flex-col gap-4">
+                <SignupForm {formId} {action} state={form?.state} />
+                <Button type="submit" form={formId}>Sign up</Button>
+                <div class="text-center text-sm">
+                    Already have an account? <a href="/auth/login" class="hover:underline">Login</a>
+                </div>
+            </div>
+        </Card.Content>
+    </Card.Root>
+</div>
