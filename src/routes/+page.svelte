44af49b77c4b6f3df44c3de0e8147057ea75a0bc
lilamaris <lilamaris.dev@gmail.com>
2025-06-14 02:32:26 +0900

    feat: update main page to display user session information and add logout functionality

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    import Button from '$components/ui/button/button.svelte';
+
+    let { data } = $props();
+</script>
+
+<div class="flex h-screen flex-col items-center justify-center">
+    <h1>Welcome, {data.user.username}!</h1>
+    <p>Session: {data.session.id}</p>
+    <pre>{JSON.stringify(data, null, 2)}</pre>
+    <Button
+        onclick={() => {
+            fetch('/auth/logout', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+        }}>Logout</Button
+    >
+</div>
