6ea9903b16f26087d42e3d05e81825adc951b7e8
lilamaris <lilamaris.dev@gmail.com>
2025-06-15 00:44:14 +0900

    test: update session validation tests and comment out page component tests

--- src/lib/server/api/session.test.ts
+++ src/lib/server/api/session.test.ts
@@ -1,8 +1,13 @@
 import prisma from '$lib/prisma';
 import { describe, it, expect } from 'vitest';
+import { createSession, generateSessionToken, validateSessionToken } from './session';
 
 describe('session', () => {
-    it('should validate a session token', async () => {
-        
-    })
-})
+    it('should get null when invalid session token', async () => {
+        const sessionToken = generateSessionToken();
+
+        const result = await validateSessionToken(sessionToken);
+        expect(result.session).toBeNull();
+        expect(result.user).toBeNull();
+    });
+});
