66d70667d4bb2de85e7b7c989d3f98153815e169
lilamaris <lilamaris.dev@gmail.com>
2025-06-14 21:05:21 +0900

    feat: enhance logout functionality by validating session token and invalidating user sessions

--- src/routes/auth/logout/+server.ts
+++ src/routes/auth/logout/+server.ts
@@ -1,8 +1,24 @@
-import { deleteSessionTokenCookie } from '$lib/server/api/session';
+import {
+    deleteSessionTokenCookie,
+    invalidateUserSessions,
+    validateSessionToken
+} from '$lib/server/api/session';
 import type { Cookies } from '@sveltejs/kit';
 import { redirect } from '@sveltejs/kit';
 
 export async function GET({ cookies }: { cookies: Cookies }) {
+    const token = cookies.get('session');
+    if (!token) {
+        return redirect(302, '/');
+    }
+
+    const { user } = await validateSessionToken(token);
+    if (!user) {
+        console.error('Invalid session token:', token);
+        return redirect(302, '/');
+    }
+
+    await invalidateUserSessions(user.id);
     deleteSessionTokenCookie(cookies);
     return redirect(302, '/');
 }
