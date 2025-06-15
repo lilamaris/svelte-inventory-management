123798193759a8de429146230bf9fa991b6b034c
lilamaris <lilamaris.dev@gmail.com>
2025-06-16 00:55:01 +0900

    feat: add format-time component for displaying formatted time in the table

---
+++ src/lib/components/table/format-time.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    let { time }: { time: Date } = $props();
+</script>
+
+<div>
+    {time.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', second: '2-digit' })}
+</div>
